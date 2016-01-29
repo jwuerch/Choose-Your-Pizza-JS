653947e2b4809b081f8bd8e47a58da81b828cb2d
Jason Wuerch <wuerchjason@gmail.com>
2016-01-29 09:45:54 -0800

    add click functionality that displays pizza size selected

--- js/scripts.js
+++ js/scripts.js
@@ -34,10 +34,39 @@ Pizza.prototype.removeTopping = function(topping) {
 
 /************************ jQuery *******************************/
 
-/** List of Toppings **/
-var pineapple = new Topping("pineapple", 2.50);
-var pepperoni = new Topping("pepperoni", 2);
-var mushrooms = new Topping("mushrooms", 1.50);
-var sausage = new Topping("sausage", 3);
-var chorizo = new Topping("chorizo", 4);
-var bacon = new Topping("bacon", 2);
+$(document).ready(function() {
+  /** List of Toppings **/
+  var pineapple= new Topping("pineapple", 2.50);
+  var pepperoni = new Topping("pepperoni", 2);
+  var mushrooms = new Topping("mushrooms", 1.50);
+  var sausage = new Topping("sausage", 3);
+  var chorizo = new Topping("chorizo", 4);
+  var bacon = new Topping("bacon", 2);
+
+  $("#sizeBtn").click(function() {
+    basePrice = 0
+    var input1 = $("#choice").val();
+    var testPizza = new Pizza(input1);
+    var testPizzaPrice = $(testPizza.sizePrice(input1));
+    $(".spanSize").text(testPizza.diameter)
+    console.log("base price is " + basePrice)
+  })
+
+
+  $("#addTopBtn").click(function(event) {
+    $(".display-topping").empty();
+      var toppingArray = []
+    $(":checkbox:checked").each(function(i){
+      toppingArray[i] = $(this).val();
+    });
+    $(".display-topping").show();
+    $(toppingArray).each(function(i){
+      $(".display-topping").append("<li>" + toppingArray[i] + "</li>");
+    });
+    console.log(toppingArray);
+
+
+    event.preventDefault();
+  });
+
+});
