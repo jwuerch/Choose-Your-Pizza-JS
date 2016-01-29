042d8d1611e8f563d8bdbc343265eb95ac369e3c
Jason Wuerch <wuerchjason@gmail.com>
2016-01-28 20:43:25 -0800

    create variables for toppings

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,6 @@ function Topping(toppingName, price) {
 };
 
 
-// var pineapple = new Topping("pineapple", 2.50);
-// var pepperoni = new Topping("pepperoni", 2);
-// var mushrooms = new Topping("mushrooms", 1.50);
-// var sausage = new Topping("sausage", 3);
-// var chorizo = new Topping("chorizo", 4);
-// var bacon = new Topping("bacon", 2);
-
-
 Pizza.prototype.sizePrice = function() {
   var pizzaPrice = basePrice;
   if (this.diameter === "small") {
@@ -37,3 +29,12 @@ Pizza.prototype.addTopping = function(topping) {
 Pizza.prototype.removeTopping = function(topping) {
   return basePrice -= topping.price
 }
+
+========jQuery==========
+
+var pineapple = new Topping("pineapple", 2.50);
+var pepperoni = new Topping("pepperoni", 2);
+var mushrooms = new Topping("mushrooms", 1.50);
+var sausage = new Topping("sausage", 3);
+var chorizo = new Topping("chorizo", 4);
+var bacon = new Topping("bacon", 2);
